0de90db1cb9bd141316eb3e45a005987d0855f84
Anyelica-ortiz01 <168257402+Anyelica-ortiz01@users.noreply.github.com>
2024-05-26 08:57:35 -0600

    Add files via upload

---
+++ App.java
@@ -0,0 +1,28 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+
+
+        Auto auto = new Auto (marca: "Toyota", modelo: "Corolla", año: 2020, kilometraje: 15000, numPasajeros:5, tipoCombustible "Gasolina");
+        Camion camion= new Camion(marca: "Volvo", modelo: "FH", año: 2019, kilometraje: 75000, capacidadCarga...20000, 4);
+
+       System.out.println(x: "Información del Auto:"); 
+       auto.mostrarInfo();
+
+
+       System.out.println(x:"\nInformación del Camion:"); 
+       camion.mostrarInfo();
+
+
+
+
+
+
+
+
+
+
+        
+    }
+}
